0c5d6bd844c144613111afeb174c59642e09fcb7
garachviraj <garachviraj@gmail.com>
2026-07-22 14:36:13 +0530

    Change Finally 4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     preset: "vercel",
   },
   define: {
-    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL || "http://localhost:3001/api"),
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL || "https://king-backend-phi.vercel.app/api"),
   },
   plugins: [
     {
